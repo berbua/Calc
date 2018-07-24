02f3f4313efc7384728dcefdb83f86389e5a1fb6
Katarzyna Berbeka <katarzyna.berbeka@gmail.com>
2018-07-24 08:44:42 +0200

    logic

---
+++ script.js
@@ -0,0 +1,47 @@
+const num = document.querySelectorAll(".num");
+const display = document.getElementById("displayer");
+const on = document.getElementById("on");
+const off = document.getElementById("off");
+let currentValue = "";
+let status = "";
+let displayValueArray = [];
+
+let displayValue = "";
+
+/*Clear*/
+on.addEventListener(
+  "click",
+  (clear = () => {
+    displayValue = 0;
+    currentValue = 0;
+    display.innerHTML = displayValue;
+    console.log("clear");
+  })
+);
+
+/*Turn off*/
+
+off.addEventListener(
+  "click",
+  (turnOff = () => {
+    display.innerHTML = "";
+    status = "off";
+  })
+);
+
+/*Clikable buttons*/
+console.log(num);
+
+num.forEach(numb =>
+  numb.addEventListener("click", function() {
+    if (currentValue === "" || currentValue === 0) {
+      currentValue = this.innerHTML;
+      displayValue = currentValue;
+      display.innerHTML = displayValue;
+    } else {
+      displayValueArray = Array.of(displayValue);
+      displayValueArray.push(currentValue);
+      console.log(displayValueArray);
+    }
+  })
+);
