1abacdeeaf87f70e7380754438429efd0112d09e
berbua <katarzyna.berbeka@gmail.com>
2018-07-25 16:20:41 +0200

    buttons working, operating not

--- script.js
+++ script.js
@@ -2,20 +2,25 @@ const num = document.querySelectorAll(".num");
 const display = document.getElementById("displayer");
 const on = document.getElementById("on");
 const off = document.getElementById("off");
+const equals = document.getElementById("equals");
 let currentValue = "";
 let status = "";
+let retultConverted = "";
 let displayValueArray = [];
 
 let displayValue = "";
+
+function displayUpdate(displayValue, currentValue) {
+  displayValue = currentValue;
+  display.innerHTML = displayValue;
+}
 
 /*Clear*/
 on.addEventListener(
   "click",
   (clear = () => {
-    displayValue = 0;
     currentValue = 0;
-    display.innerHTML = displayValue;
-    console.log("clear");
+    displayUpdate(displayValue, currentValue);
   })
 );
 
@@ -24,24 +29,51 @@ on.addEventListener(
 off.addEventListener(
   "click",
   (turnOff = () => {
-    display.innerHTML = "";
+    currentValue = "";
+    displayUpdate(displayValue, currentValue);
     status = "off";
   })
 );
 
 /*Clikable buttons*/
-console.log(num);
+// console.log(num);
 
 num.forEach(numb =>
   numb.addEventListener("click", function() {
     if (currentValue === "" || currentValue === 0) {
       currentValue = this.innerHTML;
-      displayValue = currentValue;
-      display.innerHTML = displayValue;
+      displayUpdate(displayValue, currentValue);
     } else {
       currentValue += this.innerHTML;
-      displayValue = currentValue;
-      display.innerHTML = displayValue;
+      displayUpdate(displayValue, currentValue);
+      // console.log(currentValue);
+      // console.log(typeof currentValue);
     }
   })
 );
+
+function convertToNumber(currentValue) {
+  return Number(currentValue.replace(/"/g, ""));
+  // return Number(currentValue);
+}
+
+/* Operate */
+// console.log(equals);
+// console.log(Number(currentValue));
+equals.addEventListener("click", function() {
+  displayValue = convertToNumber(currentValue);
+  console.log(displayValue);
+  display.innerHTML = displayValue;
+
+  //displayValue = currentValue;
+});
+
+// displayValue = parseFloat(currentValue.replace(/"/g, ""));
+// console.log(displayValue);
+// display.innerHTML = displayValue;
+// displayValue = parseFloat(currentValue);
+// console.log(displayValue);
+// display.innerHTML = parseFloat(currentValue);
+
+console.log(convertToNumber(3 + 3));
+console.log(currentValue);
